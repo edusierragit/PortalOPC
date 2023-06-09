a701f2d2d4c4bbb79e2a8e71534b4e45bdd38c7b
Edgardo N Moretti <latiturargentina@gmail.com>
2023-06-09 08:54:45 -0300

    agrego el backend strapi

--- pages/api/strapiconfig.js
+++
@@ -1,5 +0,0 @@
-// strapiconfig.js
-export default {
-  apiUrl: 'http://localhost:1337/admin/content-manager/collectionType/api::nota.nota/1',
-  // agregar confgs, aun no precisamos usar esto.
-}
--- strapiLoader.tsx
+++
@@ -1,7 +0,0 @@
-import { ImageLoaderProps } from 'next/image';
-
-const strapiLoader = ({ src, width, quality }: ImageLoaderProps) => {
-  return `http://localhost:1337${src}?w=${width}&q=${quality || 75}`;
-};
-
-export default strapiLoader;
